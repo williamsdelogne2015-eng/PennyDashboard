39d6be1be3631d5f1c82e3d3f42de27822783f9b
williamsdelogne2015-eng <williamsdelogne2015@gmail.com>
2025-08-30 21:15:06 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 API_KEYS = {
-    "finnhub": "TA_CLE_FINNHUB",
-    "telegram_token": "TON_BOT_TOKEN",
-    "telegram_chat_id": "TON_CHAT_ID"
+    "finnhub": "d2pkmspr01qnf9nlgibgd2pkmspr01qnf9nlgic0",
+    "telegram_token": "8328539120:AAHajRw7tkt9fFjOsk8lY0sWQ19Uu_cMWXg",
+    "telegram_chat_id": "7133695475"
 }
